f3890d231b33782f04686ce1fa5da61ab06a4263
Tareq <tareqnmd@gmail.com>
2024-04-10 12:39:11 +0600

    new nav link added

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,10 +1,18 @@
+import Link from 'next/link';
 import Logo from './Logo';
 import ModeToggle from './ModeToggle';
 
 const Navbar = () => {
 	return (
-		<nav className="container flex items-center justify-between py-4 lg:py-8">
+		<nav className="container flex items-center gap-4 lg:gap-6 justify-between py-4 lg:py-8">
 			<Logo />
+			<Link
+				className="ml-auto"
+				href="/about"
+			>
+				about
+			</Link>
+			<Link href="/blogs">blogs</Link>
 			<ModeToggle />
 		</nav>
 	);
